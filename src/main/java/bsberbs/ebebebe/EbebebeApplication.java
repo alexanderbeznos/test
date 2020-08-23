96b9e2e438f5fc4fb49cb37cd4fb2bd397478e89
alexanderbeznos <ast1bn@mail.ru>
2020-08-23 15:23:27 +0300

    0

---
+++ src/main/java/bsberbs/ebebebe/EbebebeApplication.java
@@ -0,0 +1,13 @@
+package bsberbs.ebebebe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbebebeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbebebeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bsberbs/ebebebe/EbebebeApplicationTests.java
@@ -0,0 +1,13 @@
+package bsberbs.ebebebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbebebeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
